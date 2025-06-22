173dff4dc52d2b27ec16daf16d0d3767897e0d1f
Abdulatif <abdulatif@gmail.com>
2025-06-22 10:59:21 +0600

    add

--- js/request.js
+++ js/request.js
@@ -4,22 +4,42 @@ const API = "https://randomuser.me/api/?results=9";
 // for leader
 const overlay = document.getElementById("overlay");
 
+const loaderTogel = (toggle) => {
+  if (toggle) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+};
+
 const getData = (resourse) => {
   return new Promise((resolve, rejects) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener("readystatechange", () => {
       if (request.readyState < 4) {
-        console.log("loading !");
+        loaderTogel(true);
       } else if (request.readyState == 4 && request.status == 200) {
         const data = JSON.parse(request.responseText);
-        console.log(data.results);
+        resolve(data.results);
+        loaderTogel(false);
       } else if (request.readyState == 4) {
-        console.log("Error ");
+        rejects("Error ");
+        loaderTogel(false);
       }
     });
     request.open("GET", resourse);
     request.send();
   });
 };
-getData(API);
+
+const reload = () => {
+  getData(API)
+    .then((data) => {
+      UpdateUi(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+document.addEventListener("DOMContentLoaded", reload);
